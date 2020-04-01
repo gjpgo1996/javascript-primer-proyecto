a363dae999cf87b04feb030fccce538b1f8ebdfc
gjpgo1996 <gjpgot@gmail.com>
2020-04-01 00:09:42 +0000

    javascript tutorial

--- app.js
+++ app.js
@@ -1,1 +1,139 @@
-document.write("<h1>Hello World!</h1>");
+/* // ESTOY CREANDO VARIABLES
+
+var nameUser = "john";
+let lastName = "cartel";
+
+//REASIGNANDO EL VALOR DE LA VARIABLE nameUser
+
+nameUser = "pepe";
+
+//cualquier cosa
+
+const PI = 3.1415;
+const id = "user1";
+
+console.log(PI)
+
+// CAMELCASE
+
+let nombreDePersona = "gordon";
+
+*/
+
+/*
+let name = "John";
+let lastName = "Carter";
+
+let completeName = name + " " + lastName;
+
+console.log(completeName);
+*/
+
+/*
+let numberOne = 100;
+let numberTwo = 500;
+
+let result = numberOne >= numberTwo;
+console.log(result);
+*/
+
+
+/*
+let passwordDB = 'pepe123';
+let input = 'pepe123';
+
+let result = input == passwordDB;
+
+if (result == true) {
+    console.log("logging correcto");
+} else {
+    console.log("logging incorrecto");
+}
+
+*/
+
+/*
+let score = 70;
+
+if (score > 30) {
+    console.log("necesitas practicar más");
+} else if (score > 15) {
+    console.log("estás mejorando");
+} else {
+    console.log("necesitas ver este tutorial")
+}
+*/
+
+/*
+let typeCard = 'Debit Card';
+
+switch(typeCard) {
+
+    case 'Debit Card': 
+        console.log('This is a debit card');
+        break;
+    case 'Credit Card':
+        console.log('This is a credit card');
+        break;
+    default:
+        console.log('No card available');
+}
+*/
+/*
+let count = 50;
+
+while (count > 0) {
+
+    console.log('Hello World');
+    count = count - 1;
+}
+*/
+/*
+let count = 50;
+
+while (count > 0) {
+
+    console.log(count);
+    count = count - 1;
+}
+*/
+
+/*
+let count = 1;
+
+while (count <= 50) {
+
+    console.log(count);
+    count++;
+}
+*/
+/*
+let names = ['Ryan', 'Joe', 'John'];
+
+console.log(names.length);
+*/
+/*
+let names = ['Ryan', 'Joe', 'John'];
+
+for (let i = names.length - 1; i >= 0; i--) {
+    console.log(names[i]);
+}
+*/
+
+/*
+function greeting(name) {
+    console.log("Hello " + name);
+}
+
+greeting('Kara');
+greeting('Markus');
+greeting('Connor');
+
+*/
+
+function add(n1, n2) {
+    console.log(n1 + n2);
+}
+
+add(3, 2);
+add(2, 'asdasd');
